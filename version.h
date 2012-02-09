ce5c7625ef96541527d7f22897c4d2776555f76d
Cedric <roman@numengo.com>
2012-02-09 23:27:49 +0100

    improved library versionning for non windows platform and management in numengo library manager removed useless scripts for build
    Signed-off-by: Cedric <roman@numengo.com>

---
+++ version.h
@@ -0,0 +1,16 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define RC_FILENAME "muParser\0"
+#define RC_DESCRIPTION_STRING "numenGo version of muParser\0"
+#define RC_FILEVERSION 1,3,0,0
+#define RC_FILEVERSION_STRING "1.3rev0\0"
+
+#define RC_PRODUCTNAME_STRING "numenGo|Toolbox\0"
+#define RC_PRODUCTVERSION 12,02,0,0
+#define RC_PRODUCTVERSION_STRING "12.02rev0\0"
+
+#define RC_COMPANYNAME "NUMENGO\0"
+#define RC_COPYRIGHT_STRING "Copyright (c) 2008 Ingo Berg\0"
+
+#endif //VERSION_H
